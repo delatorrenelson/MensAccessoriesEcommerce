6fbf88526969ff635b014e308390ce6ccf167743
Nelson Dela Torre <delatorrenelson127@gmail.com>
2022-04-01 16:51:19 +0800

    Fixing user view search...

--- src/pages/UserView.js
+++ src/pages/UserView.js
@@ -2,87 +2,80 @@ import { useState, useEffect, useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FaSearch } from "react-icons/fa";
-import {Container, Row, Col, InputGroup, FormControl} from "react-bootstrap"
+import { Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
 import ProductCard from "../components/ProductCard";
-import Loader from "../components/Loader"
+import Loader from "../components/Loader";
 
-import "./UserView.scss"
+import "./UserView.scss";
 
-export default function UserView(props){
-    const [products, setProducts] = useState([]);      
-    const [keyword, setKeyWord] = useState("");  
-    const [isMounted, setIsMounted] = useState(true)
-    
-    const fetchData = () => {
-      fetch(`${process.env.REACT_APP_API_URL}/products`)
+export default function UserView(props) {
+  const [products, setProducts] = useState([]);
+  const [keyword, setKeyWord] = useState("");
+  const [isMounted, setIsMounted] = useState(true);
+
+  const fetchData = () => {
+    fetch(`${process.env.REACT_APP_API_URL}/products`)
       .then((res) => res.json())
       .then((data) => {
         if (data) {
-          let activeProduct = data.filter((product) => product.isActive === true)
-          setProducts(activeProduct);                       
+          let activeProduct = data.filter(
+            (product) => product.isActive === true
+          );
+          setProducts(activeProduct);
         }
-      });        
-      };
-    
-      const searchProduct = (e) => {
-        console.log(e)
-        if(e !== ""){
-          fetch(`${process.env.REACT_APP_API_URL}/products/q/${e}`)
-          .then((res) => res.json())
-          .then((data) => {
-            if (data) {
-              setProducts(data);        
-            }
-          });
-        }else{
-          fetchData()
-        }
-      };        
+      });
+  };
 
-      useEffect(() => {
-        
-        if(isMounted){
-          fetchData()
-        }            
-        
-        return () => {
-          setIsMounted(false)
-        }
+  const searchProduct = (e) => {
+    console.log(e);
+    if (e !== "") {
+      fetch(`${process.env.REACT_APP_API_URL}/products/q/${e}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data) {
+            setProducts(data);
+          }
+        });
+    } else {
+      fetchData();
+    }
+  };
 
-        
-      }, [products]);
-    
-      const productList = products.map((product) => (
-        <ProductCard key={product._id} product={product} />
-      ));    
+  useEffect(() => {
+    if (isMounted) {
+      fetchData();
+    }
 
-    return (
-        <Container className={"my-3"}>
-        <Row>
-          <Col>
-            <InputGroup>
-              <InputGroup.Text id="btnGroupAddon2">
-                <FaSearch />
-              </InputGroup.Text>
-              <FormControl
-                onChange={(e) => searchProduct(e)}
-                // onKeyUp={(e) => getProducts(e)}
-                type="text"
-                placeholder="Search..."
-                aria-label="Search..."
-              />
-            </InputGroup>
-          </Col>
-        </Row>
-  
-        <Row className="">
-          {productList.length === 0 ? (
-            
-            <Loader/>
-          ) : (
-            productList
-          )}
-        </Row>
-      </Container>
-    )
+    return () => {
+      setIsMounted(false);
+    };
+  }, [products]);
+
+  const productList = products.map((product) => (
+    <ProductCard key={product._id} product={product} />
+  ));
+
+  return (
+    <Container className={"my-3"}>
+      <Row>
+        <Col>
+          <InputGroup>
+            <InputGroup.Text id="btnGroupAddon2">
+              <FaSearch />
+            </InputGroup.Text>
+            <FormControl
+              onChange={(e) => searchProduct(e)}
+              type="text"
+              placeholder="Search..."
+              aria-label="Search..."
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+
+      <Row className="">
+        {productList.length === 0 ? <Loader /> : productList}
+      </Row>
+    </Container>
+  );
 }
